8b232e73729b8671ce1cbf39dedbc73b44e5ea77
aonnadi <aninja1008@gmail.com>
2023-12-01 08:58:52 -0500

    get app to work

--- ps4-express/app.js
+++ ps4-express/app.js
@@ -3,12 +3,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const ps4Router = require('./routes/ps4');
+var path = require('path');
 
-app.set('view engine', 'ejs'); // or 'pug' if you prefer Pug as the template engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug'); // or 'pug' if you prefer Pug as the template engine
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/ps4', ps4Router);
+app.use('/ps4', ps4Router); // Mount the ps4Router on '/ps4' path
+// app.use('/ps4', ps4Router()); - this doesnt work
+
+// need this for it to work on /
+app.get('/', (req, res) => {
+    res.render('pugfile'); // Render your Pug template here
+  });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
---
+++ ps4-express/config.js
@@ -0,0 +1,5 @@
+// config.js
+module.exports = {
+    lastFmApiKey: '28c57d10eebe74fd2c9096cfab627fff',
+  };
+  
